3462d179a356f295e3d9f5385ffd74ff83e1177c
NatarBL <nateblandofficial@gmail.com>
2024-05-28 17:44:08 -0400

    Fix data export bug

--- server/venv/main.py
+++ server/venv/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from read_excel_file import data
-
+import json
 
 app = Flask(__name__)
 cors = CORS(app, origins='*') ## Specify where we are accepting requests from
@@ -9,7 +9,7 @@ cors = CORS(app, origins='*') ## Specify where we are accepting requests from
 @app.route("/api/users", methods=['GET'])
 
 def users():
-    return data
+    return json.dumps(data)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
