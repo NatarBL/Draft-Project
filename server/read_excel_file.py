ba2698fcaa133b07bf749e6276d1965e57ff33dc
NatarBL <nateblandofficial@gmail.com>
2024-06-19 12:47:01 -0400

    remove venv

--- server/read_excel_file.py
+++ server/read_excel_file.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import json
 
-df = pd.read_excel(r".\venv\Data\Data_Book.xlsx")
+df = pd.read_excel(r".\Data\Data_Book.xlsx")
 
 s_array = df[["week1","week2","week3","week4","week5","week6","week7","week8","week9","week10","week11","week12","week13","week14","week15","week16","week17"]].to_numpy()
 
-df = pd.read_excel(r".\venv\Data\Data_Book.xlsx", usecols="A:L") # reads values from columns A-L
+df = pd.read_excel(r".\Data\Data_Book.xlsx", usecols="A:L") # reads values from columns A-L
 
 df['ppr_by_week'] = [s_array[0],s_array[1],s_array[2],s_array[3],s_array[4],s_array[5],s_array[6],s_array[7],s_array[8],s_array[9],s_array[10],s_array[11],s_array[12],s_array[13],s_array[14],s_array[15],s_array[16],s_array[17],s_array[18],s_array[19]]
 
